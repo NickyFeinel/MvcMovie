0228417666971b21296c1dd37503902eac086a24
Nicole Feinel <lourdesfeinel@gmail.com>
2025-04-04 14:26:29 -0300

    Agregado y corrección en el controlador

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+public class HelloWorldController : Controller
+{
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+
+    // GET: /HelloWorld/Welcome/
+    public string Welcome(string name, int numTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($" Hello {name}, NumTimes is: {numTimes}");
+    }
+}
